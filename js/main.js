8747c74eb45824b4b0fd48b7c1d89b66b3e60d4c
synesdeija <esemde@pm.me>
2022-04-10 14:14:43 -0500

    completed html, css, javascript. requires paid key

--- js/main.js
+++ js/main.js
@@ -2,39 +2,57 @@
 
 //Do: Find three APIs and build three simple apps using those APIs (Not all of these work, but it is a start: https://github.com/public-apis/public-apis)
 
-document.querySelector('button').addEventListener('click', getFetch)
+// document.querySelector('button').addEventListener('click', getFetch)
 
-function getFetch() {
-    const choice = document.querySelector('input').value
-    const url = 'https://pokeapi.co/api/v2/pokemon/' + choice
+// function getFetch() {
+//     const choice = document.querySelector('input').value
+//     const url = 'https://pokeapi.co/api/v2/pokemon/' + choice
 
-    fetch(url)
-        .then(res => res.json()) // parse response as JSON
+//     fetch(url)
+//         .then(res => res.json()) // parse response as JSON
+//         .then(data => {
+//             console.log(data)
+//         })
+//         .catch(err => {
+//             console.log(`error ${err}`)
+//         });
+// }
+
+
+// document.querySelector('button').addEventListener('click', getDrink)
+
+// function getDrink() {
+//     let drink = document.querySelector('input').value
+
+//     fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${drink}`)
+//         .then(res => res.json()) //saying:parse response as json
+//         .then(data => {
+//             console.log(data.drinks[0])
+//             document.querySelector('h2').innerText = data.drinks[0].strDrink
+//             document.querySelector('img').src = data.drinks[0].strDrinkThumb
+//             document.querySelector('h3').innerText = data.drinks[0].strInstructions
+
+//         })
+
+//     .catch(err => {
+//         console.log(`error ${err}`)
+//     })
+// }
+
+document.querySelector('button').addEventListener('click', getLatLon)
+
+function getLatLon() {
+    let lat = document.querySelector('.lat').value
+    let lon = document.querySelector('.lon').value
+
+    fetch(`https://api.breezometer.com/pollen/v2/forecast/daily?lat=${lat}&lon=${lon}&key=1&features=types_information,plants_information&days=3`)
+        .then(res => res.json())
         .then(data => {
-            console.log(data)
+            console.log(data.dailyForecast)
+            document.querySelector('h3').innerText = data.strDailyForecast
+
         })
         .catch(err => {
             console.log(`error ${err}`)
-        });
-}
-
-
-document.querySelector('button').addEventListener('click', getDrink)
-
-function getDrink() {
-    let drink = document.querySelector('input').value
-
-    fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${drink}`)
-        .then(res => res.json()) //saying:parse response as json
-        .then(data => {
-            console.log(data.drinks[0])
-            document.querySelector('h2').innerText = data.drinks[0].strDrink
-            document.querySelector('img').src = data.drinks[0].strDrinkThumb
-            document.querySelector('h3').innerText = data.drinks[0].strInstructions
-
         })
-
-    .catch(err => {
-        console.log(`error ${err}`)
-    })
 }
