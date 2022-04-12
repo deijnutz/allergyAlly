fea65a433c87f5e5850ba94862cb92fc2b029e1d
synesdeija <esemde@pm.me>
2022-04-12 09:35:47 -0500

    working on fetch issue

--- js/main.js
+++ js/main.js
@@ -1,43 +1,8 @@
-//fetch using BreezoMeter using latitude, longitude, day count, and api key.
+//fetch pollen count forecast with BreezoMeter API intaking latitude & longitude, day count, and api key.
 
 //Do: Find three APIs and build three simple apps using those APIs (Not all of these work, but it is a start: https://github.com/public-apis/public-apis)
 
-// document.querySelector('button').addEventListener('click', getFetch)
 
-// function getFetch() {
-//     const choice = document.querySelector('input').value
-//     const url = 'https://pokeapi.co/api/v2/pokemon/' + choice
-
-//     fetch(url)
-//         .then(res => res.json()) // parse response as JSON
-//         .then(data => {
-//             console.log(data)
-//         })
-//         .catch(err => {
-//             console.log(`error ${err}`)
-//         });
-// }
-
-
-// document.querySelector('button').addEventListener('click', getDrink)
-
-// function getDrink() {
-//     let drink = document.querySelector('input').value
-
-//     fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${drink}`)
-//         .then(res => res.json()) //saying:parse response as json
-//         .then(data => {
-//             console.log(data.drinks[0])
-//             document.querySelector('h2').innerText = data.drinks[0].strDrink
-//             document.querySelector('img').src = data.drinks[0].strDrinkThumb
-//             document.querySelector('h3').innerText = data.drinks[0].strInstructions
-
-//         })
-
-//     .catch(err => {
-//         console.log(`error ${err}`)
-//     })
-// }
 
 document.querySelector('button').addEventListener('click', getLatLon)
 
@@ -45,11 +10,11 @@ function getLatLon() {
     let lat = document.querySelector('.lat').value
     let lon = document.querySelector('.lon').value
 
-    fetch(`https://api.breezometer.com/pollen/v2/forecast/daily?lat=${lat}&lon=${lon}&key=1&features=types_information,plants_information&days=3`)
+    fetch(`https://api.breezometer.com/pollen/v2/forecast/daily?lat=${lat}&lon=${lon}&key=34f470133dd84be7a0e67ff4ded7773a&features=types_information,plants_information&days=3&metadata=true`)
         .then(res => res.json())
         .then(data => {
-            console.log(data.dailyForecast)
-            document.querySelector('h3').innerText = data.strDailyForecast
+            console.log(data.getLatLon)
+            document.querySelector('h3').innerText = data.getLatLon
 
         })
         .catch(err => {
